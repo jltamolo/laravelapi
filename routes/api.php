d4566709aa97b509e9a08c5825bfeb1325ab5257
jltamolo <jltamolo@gmail.com>
2023-12-30 16:33:03 +0300

    sanctum authentication

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TrialController;
 use App\Http\Controllers\DeviceController;
 use App\Http\Controllers\MemberController;
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -19,13 +20,21 @@ use App\Http\Controllers\MemberController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get("data", [TrialController::class, 'getData']);
-Route::get("list/{id?}", [DeviceController::class, 'list']);
-Route::post("add",[DeviceController::class,'add']);
-Route::put("update",[DeviceController::class,'update']);
-Route::get("search/{device_name}",[DeviceController::class,'search']);
-Route::delete("delete/{id}",[DeviceController::class,'delete']);
-Route::delete('devices/delete-multiple', [DeviceController::class, 'deleteMultiple']);
-Route::post("save",[DeviceController::class,'testData']);
+Route::group(['middleware' => 'auth:sanctum'], function(){
+    //All secure URL's
+    Route::apiResource("member", MemberController::class);
+    Route::get("data", [TrialController::class, 'getData']);
+    Route::get("list/{id?}", [DeviceController::class, 'list']);
+    Route::post("add",[DeviceController::class,'add']);
+    Route::put("update",[DeviceController::class,'update']);
+    Route::get("search/{device_name}",[DeviceController::class,'search']);
+    Route::delete("delete/{id}",[DeviceController::class,'delete']);
+    Route::delete('devices/delete-multiple', [DeviceController::class, 'deleteMultiple']);
+    Route::post("save",[DeviceController::class,'testData']);
 
-Route::apiResource("member", MemberController::class);
+
+    });
+
+
+
+Route::post("login",[UserController::class,'index']);
